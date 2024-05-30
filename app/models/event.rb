3cbce52e3fe7fe56a3ed37357317848fc6bbc6ba
Dustin Goodman <dustin@thisdot.co>
2024-05-30 16:09:27 -0500

    wip

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,8 @@ class Event < ApplicationRecord
   enum venue_type: { in_person: "IN_PERSON", online: 'ONLINE', hybrid: 'HYBRID' }
   has_many :event_sessions, dependent: :destroy
   has_many :event_speakers, dependent: :destroy
+
+  # validates :name, presence: true
+  # validates :start_at, presence: true
+  # validates :end_at, presence: true, date: { after: :start_at }
 end
