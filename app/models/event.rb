bb8aaa43fd199f2d7bb32356571cff236ecefabe
Dustin Goodman <dustin@thisdot.co>
2024-05-30 20:47:30 -0500

    add validation to events

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,7 +6,18 @@ class Event < ApplicationRecord
   has_many :event_speakers, dependent: :destroy
   belongs_to :platform_organization
 
-  # validates :name, presence: true
-  # validates :start_at, presence: true
-  # validates :end_at, presence: true, date: { after: :start_at }
+  validates :name, presence: true
+  validate :dates
+
+  private
+
+  def dates
+    errors.add(:start_at, 'must be present') if start_at.blank?
+    errors.add(:end_at, 'must be present') if end_at.blank?
+
+    return if errors.present?
+
+    errors.add(:start_at, 'must be before the end date') if start_at > end_at
+    errors.add(:start_at, 'must be in the future') if start_at < Time.zone.now
+  end
 end
