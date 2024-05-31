79435c0138db30f6d15e0b50d34527da7e9b7ed9
Dustin Goodman <dustin@thisdot.co>
2024-05-30 20:47:39 -0500

    starting to make tests work

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -3,7 +3,9 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not save event without name' do
+    org = PlatformOrganization.create(name: 'Test Organization')
+    event = Event.new(platform_organization_id: org.id, start_at: 1.minute.from_now, end_at: 1.day.from_now, venue_type: 'IN_PERSON')
+    assert_not event.save, 'Saved the event without a name'
+  end
 end
