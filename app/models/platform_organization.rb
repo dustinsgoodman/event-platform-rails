a764e7bd21ba06a8d4a458b4650ccdceb7cb4c8d
Dustin Goodman <dustin@thisdot.co>
2024-06-13 18:42:15 -0500

    clear current org on users if org is deleted

--- app/models/platform_organization.rb
+++ app/models/platform_organization.rb
@@ -6,4 +6,14 @@ class PlatformOrganization < ApplicationRecord
   has_many :events, dependent: :destroy
 
   validates :name, presence: true
+
+  before_destroy :clear_users_current_organization, prepend: true
+
+  private
+
+  def clear_users_current_organization
+    users
+      .where(current_platform_organization: self)
+      .update_all(current_platform_organization_id: nil) # rubocop:disable Rails/SkipsModelValidations
+  end
 end
