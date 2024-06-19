63788c89346652f116a81353f5568929f2790778
Dustin Goodman <dustin@thisdot.co>
2024-06-19 16:46:38 -0500

    add navigation tree for dynamic nav

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,5 +4,43 @@ class AdminController < ApplicationController
   protect_from_forgery prepend: true
   before_action :authenticate_user!
 
-  layout 'admin'
+  def navigation_tree
+    @current_user = User.joins(%i[platform_organizations current_platform_organization]).find(current_user.id)
+    @current_platform_organization = @current_user.current_platform_organization
+    @platform_organizations = @current_user.platform_organizations
+
+    [
+      {
+        title: 'Events',
+        path: events_path
+      },
+      {
+        title: 'User Management',
+        path: '#'
+      },
+      {
+        title: @current_platform_organization.try(:name) || 'No Organization Found',
+        submenu: @platform_organizations.filter_map do |platform_organization|
+          if platform_organization != @current_platform_organization
+            {
+              title: platform_organization.name,
+              path: change_current_organization_users_path(id: platform_organization.id),
+              method: :post
+            }
+          end
+        end
+      },
+      {
+        title: @current_user.email,
+        submenu: [
+          {
+            title: 'Log out',
+            path: destroy_user_session_path,
+            method: :delete
+          }
+        ]
+      }
+    ]
+  end
+  helper_method :navigation_tree
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,49 @@ class ApplicationController < ActionController::Base
   def not_found
     raise ActionController::RoutingError, 'Not Found'
   end
+
+  def navigation_tree # rubocop:disable Metrics/MethodLength
+    tree = [
+      {
+        title: 'Products',
+        submenu: [
+          { title: 'Product 1', path: '#' },
+          { title: 'Product 2', path: '#' },
+          { title: 'Product 3', path: '#' },
+          { title: 'Product 4', path: '#' }
+        ]
+      },
+      {
+        title: 'About',
+        submenu: [
+          { title: 'About 1', path: '#' },
+          { title: 'About 2', path: '#' },
+          { title: 'About 3', path: '#' },
+          { title: 'About 4', path: '#' }
+        ]
+      }
+    ]
+    if user_signed_in?
+      tree << {
+        title: 'Dashboard',
+        path: events_path,
+        class: 'btn btn-primary'
+      }
+      tree << {
+        title: 'Log out',
+        path: destroy_user_session_path,
+        class: 'btn btn-secondary',
+        method: :delete
+      }
+    else
+      tree << {
+        title: 'Log In',
+        path: new_user_session_path,
+        class: 'btn btn-primary'
+      }
+    end
+
+    tree
+  end
+  helper_method :navigation_tree
 end
