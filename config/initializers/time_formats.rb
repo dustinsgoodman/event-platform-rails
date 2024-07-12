a3cfecdc3bc97c169b6a2102901e74d71d584c8f
Dustin Goodman <dustin@thisdot.co>
2024-07-11 20:08:39 -0500

    rubocop

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,1 +1,3 @@
+# frozen_string_literal: true
+
 Time::DATE_FORMATS[:abbrev_rfc822] = '%a %b %d, %Y %H:%M'
--- db/migrate/20240628000130_add_locale_to_user.rb
+++ db/migrate/20240628000130_add_locale_to_user.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddLocaleToUser < ActiveRecord::Migration[7.1]
   def change
     add_column :users, :locale, :string, default: 'en-US', null: false
